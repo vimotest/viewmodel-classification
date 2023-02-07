cbe0590c3e739764b393463daf041568140d14c0
Mario Fuksa <44242539+fumapps@users.noreply.github.com>
2023-02-07 21:22:08 +0100

    Main: begin logic to automatically find next TODO website and enter into markdown table

---
+++ site_initial_classification.md
@@ -0,0 +1,5 @@
+Table of results with Key, Title, URL, and Classification
+
+| Key | Title                                   | URL                                                                  | Classification |
+|-----|-----------------------------------------|----------------------------------------------------------------------|----------------|
+| -   | ViewModel overview - Android Developers | https://developer.android.com/topic/libraries/architecture/viewmodel | TODO           |
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,44 @@
-fun main(args: Array<String>) {
+import java.io.File
 
+data class Website(val url: String, val name: String)
+
+fun main(args: Array<String>) {
+    val website = getNextUrl()
+    if (website != null) {
+        println("Next website: ${website.name} (${website.url})")
+        addWebsiteToClassificationFile(website)
+    } else {
+        println("No more websites to check")
+    }
 }
+
+private fun getNextUrl(): Website? {
+    val files = File("input").listFiles()
+    for (file in files) {
+        for (line in file.readText().lines()) {
+            if (containsChineseCharacters(line)) {
+                continue
+            }
+            if (!lineIsAlreadyUsed(line)) {
+                return Website(line.toUrl(), line.toWebsiteName())
+            }
+        }
+    }
+    return null
+}
+
+private fun addWebsiteToClassificationFile(website: Website) {
+    val file = File("site_initial_classification.md")
+    if (!file.readText().contains(website.url)) {
+        file.appendText("|-|${website.name}|${website.url}|TODO|\n")
+    }
+}
+
+private fun usedUrls() = emptySet<String>()
+
+private fun lineIsAlreadyUsed(line: String) = usedUrls().contains(line.toUrl())
+
+private fun String.toUrl() = substringBefore("\t")
+private fun String.toWebsiteName() = substringAfter("\t")
+
+private fun containsChineseCharacters(line: String) = line.any { it in '\u4e00'..'\u9fa5' }
