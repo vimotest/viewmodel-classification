93c6eec153ee8f98aa67b1c0d0ae6ef86081a64d
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2023-03-04 10:43:59 +0100

    Paper Initial Classification: insert Type also

--- site_initial_classification_papers.md
+++ site_initial_classification_papers.md
@@ -1,7 +1,7 @@
-Table of results with Key, Title, URL, and Classification
+Table of results with Key, Title, Type, URL, and Classification
 
 Classification status: TODO, REJECT, ACCEPT, REVIEW
 
-| Key | Title                                                          | URL                                                                                        | Classification | Reason |
-|-----|----------------------------------------------------------------|--------------------------------------------------------------------------------------------|----------------|--------|
-| -   | Patterns-wpf apps with the model-view-viewmodel design pattern | https://scholar.google.com/scholar?cites=9777780009050957602&as_sdt=2005&sciodt=2007&hl=en | TODO           |        |
+| Key | Title                                                          | Type     | URL                                                                                        | Classification | Reason |
+|-----|----------------------------------------------------------------|----------|--------------------------------------------------------------------------------------------|----------------|--------|
+| -   | Patterns-wpf apps with the model-view-viewmodel design pattern | CITATION | https://scholar.google.com/scholar?cites=9777780009050957602&as_sdt=2005&sciodt=2007&hl=en | TODO           |        |
--- src/main/kotlin/papers/PaperLiteratureMain.kt
+++ src/main/kotlin/papers/PaperLiteratureMain.kt
@@ -2,7 +2,7 @@ package papers
 
 import java.io.File
 
-data class Paper(val url: String, val name: String)
+data class Paper(val url: String, val name: String, val type: String)
 
 fun main(args: Array<String>) {
     if (args.size == 1 && args.first() == "count") {
@@ -11,16 +11,16 @@ fun main(args: Array<String>) {
         println("$alreadyHandled of $count initially handled, remaining: ${count - alreadyHandled}")
         return
     }
-    val paper = getNextUrl()
+    val paper = getNextPaper()
     if (paper != null) {
-        println("Next paper: ${paper.name} (${paper.url})")
+        println("Next paper: ${paper.name} <${paper.type}> (${paper.url})")
         addPaperToClassificationFile(paper)
     } else {
         println("No more papers to check")
     }
 }
 
-private fun getNextUrl(): Paper? {
+private fun getNextPaper(): Paper? {
     var result: Paper? = null
     iterateInputUrls {
         val stop = !paperIsAlreadyUsed(it)
@@ -36,7 +36,7 @@ private fun paperIsAlreadyUsed(paper: Paper) = usedUrls.contains(paper.url)
 private fun addPaperToClassificationFile(paper: Paper) {
     val file = File("site_initial_classification_papers.md")
     if (!file.readText().contains(paper.url)) {
-        file.appendText("|-|${paper.name}|${paper.url}|TODO| |\n")
+        file.appendText("|-|${paper.name}|${paper.type}|${paper.url}|TODO| |\n")
     }
 }
 
@@ -44,12 +44,13 @@ private val usedUrls : Set<String> by lazy {
     File("site_initial_classification_papers.md")
         .readText()
         .lines()
-        .filter { it.matches("\\|[^|]+\\|[^|]+\\|[^|]+\\|\\s*(REJECT|REVIEW|ACCEPT|STANDARD_ACCEPT)\\s*\\|[^|]+\\|".toRegex()) }
-        .map { it.split("|")[3].trim() }.toSet()
+        .filter { it.matches("\\|[^|]+\\|[^|]+\\|[^|]+\\|[^|]+\\|\\s*(REJECT|REVIEW|ACCEPT|STANDARD_ACCEPT)\\s*\\|[^|]+\\|".toRegex()) }
+        .map { it.split("|")[4].trim() }.toSet()
 }
 
 private fun String.toUrl() = getPaperUrl()
 private fun String.toPaperName() = getTitle()
+private fun String.toUrlType() = getType().takeIf { it.isNotBlank() } ?: "DEFAULT"
 
 private fun countAllUrls(): Int {
     val urlSet = mutableSetOf<String>()
@@ -63,7 +64,7 @@ private fun countAllUrls(): Int {
 private fun iterateInputUrls(consumer: (Paper) -> Boolean) {
     val file = File("searches/relevantPapers.csv")
     for (line in file.readText().lines().drop(1)) {
-        val stop = consumer(Paper(line.toUrl(), line.toPaperName()))
+        val stop = consumer(Paper(line.toUrl(), line.toPaperName(), line.toUrlType()))
         if (stop) {
             return
         }
